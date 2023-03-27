f3fdafc1d4bbd89f083b50e8cfedee42c1578ecc
aline_hesley <alinehesley281@gmail.com>
2023-03-27 16:23:46 -0300

    revisado com instancias criadas na main

--- src/Classes/Cliente.java
+++ src/Classes/Cliente.java
@@ -1,127 +1,127 @@
 package Classes;
 
 public class Cliente {
-	//Attributes
-	private String nome;
-	private String cpf;
-	private String dataNascimento;
-	private int idade;
-	private String endereco;
-	
-	//Constructor 
-	public Cliente (String nome, String cpf, String dataNascimento, int idade, String endereco) {
-		this.nome = nome;
-		this.cpf = ApenasNumeros(cpf);
-		this.dataNascimento = dataNascimento;
-		this.idade = idade;
-		this.endereco = endereco;
-	}
-	
-	// VALIDAÇÃO DE CPF //
-	
-	public boolean validarCPF(String cpf) {
-		//cpf = ApenasNumeros(cpf);
-		//System.out.println(QuantDigitosOk(cpf));
-		//System.out.println(DigitosIguais(cpf));
-		//System.out.println(CalculaDigitos(cpf));
-		if(QuantDigitosOk(cpf) && DigitosIguais(cpf)==false && CalculaDigitos(cpf))
-			return true;
-		return false;
-		
-	}
-	
-	//Remove todos os caracteres não numericos utilizando replaceAll
-	private static String ApenasNumeros(String cpf){
-		   cpf = cpf.replaceAll("[^0-9]","");
-		   return cpf;
-		}
-	
-	//Verifica se possui 11 dígitos
-	private boolean QuantDigitosOk(String cpf){  
-	      if (cpf.length() != 11 )
-	        return false;  
-	      else {
-	    	return true;
-	      }
-	  }
-	 
-	 //Verifica se todos os dígitos são iguais	 @SuppressWarnings("unused")
-	private boolean DigitosIguais(String cpf) {
-		 int i;
-		 for (i = 1; i < cpf.length(); i++)
-		        if (cpf.charAt(0) != cpf.charAt(i))
-		          return false;
-		      return true;
-	 }
-	 
-	 //Calcula os digitos verificadores (O que seria isso?)
-	 private boolean CalculaDigitos(String cpf) {
-		 //cpf string, pego cada caractere .charAt
-		 int i;
-		 int soma_digito1 = 0;
-		 int soma_digito2 = 0;
-		 
-		 for(i = 0; i < 9; i++) {
-			 soma_digito1 += (cpf.charAt(i)-48) * (10-i);
-		 }
-		 int mod_soma1 = soma_digito1%11;
-		 int digito1 = cpf.charAt(9)-48;		 
-		 
-		 if(mod_soma1 <= 1) {
-			 if(digito1!=0)return false;
-		 }else { 
-			 if(11-mod_soma1 != digito1)
-				 return false;
-		 }
-		 for(i = 1; i < 10; i++) {
-			 soma_digito2 += (cpf.charAt(i)-48) * (10-i+1);
-		 }
-		 int mod_soma2 = soma_digito2%11;
-		 int digito2 = cpf.charAt(10)-48;
-		 
-		 if(mod_soma2 <= 1) {
-			 if(digito2!=0)return false;
-		 }else { 
-			 if(11-mod_soma2 != digito2)
-				 return false;
-		 }
-		 
-		 return true;
-	 }
-	 
-	//Getters e setters
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	
-	public int getIdade(int idade) {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	
-	public String getEndereco(String endereco) {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+  //Attributes
+  private String nome;
+  private String cpf;
+  private String dataNascimento;
+  private int idade;
+  private String endereco;
+
+  //Constructor 
+  public Cliente(String nome, String cpf, String dataNascimento, int idade, String endereco) {
+    this.nome = nome;
+    this.cpf = ApenasNumeros(cpf);
+    this.dataNascimento = dataNascimento;
+    this.idade = idade;
+    this.endereco = endereco;
+  }
+
+  // VALIDAÇÃO DE CPF //
+  public boolean validarCPF(String cpf) {
+    if (QuantDigitosOk(cpf) && DigitosIguais(cpf) == false && CalculaDigitos(cpf))
+      return true;
+    return false;
+  }
+
+  //Remove todos os caracteres não numericos utilizando replaceAll
+  private static String ApenasNumeros(String cpf) {
+    cpf = cpf.replaceAll("[^0-9]", "");
+    return cpf;
+  }
+
+  //Verifica se o cpf possui 11 dígitos
+  private boolean QuantDigitosOk(String cpf) {
+    if (cpf.length() != 11)
+      return false;
+    else {
+      return true;
+    }
+  }
+
+  //Verifica se todos os dígitos são iguais
+  private boolean DigitosIguais(String cpf) {
+    int i;
+    for (i = 1; i < cpf.length(); i++)
+      if (cpf.charAt(0) != cpf.charAt(i))
+        return false;
+    return true;
+  }
+
+  //Calcula os digitos verificadores
+  private boolean CalculaDigitos(String cpf) {
+    int i;
+    int soma_digito1 = 0;
+    int soma_digito2 = 0;
+
+    for (i = 0; i < 9; i++) {
+      soma_digito1 += (cpf.charAt(i) - 48) * (10 - i);
+    }
+    int mod_soma1 = soma_digito1 % 11;
+    int digito1 = cpf.charAt(9) - 48;
+
+    if (mod_soma1 <= 1) {
+      if (digito1 != 0) return false;
+    } else {
+      if (11 - mod_soma1 != digito1)
+        return false;
+    }
+    for (i = 1; i < 10; i++) {
+      soma_digito2 += (cpf.charAt(i) - 48) * (10 - i + 1);
+    }
+    int mod_soma2 = soma_digito2 % 11;
+    int digito2 = cpf.charAt(10) - 48;
+
+    if (mod_soma2 <= 1) {
+      if (digito2 != 0) return false;
+    } else {
+      if (11 - mod_soma2 != digito2)
+        return false;
+    }
+
+    return true;
+  }
+
+  //Retorna uma string com todos os atributos da classe Cliente
+  public String toString() {
+    return "Informações do Cliente\nNome:" + nome + "\nCPF:" + cpf + 
+    		"\nData de Nascimento:" + dataNascimento +
+    		"\nIdade:"+ idade + "\nEndereço:" + endereco + '\n';
+  }
+
+  //Getters e setters
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getDataNascimento() {
+    return dataNascimento;
+  }
+  public void setDataNascimento(String dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
 }
--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -2,12 +2,28 @@ package Classes;
 
 public class Main {
 	public static void main(String[] args) {
-		Cliente aline = new Cliente("Aline", "612977023-52", "15/02/2003", 20, "Rua Agostinho Ramalho");
-		System.out.println(aline.getCpf());
-		System.out.println(aline.validarCPF(aline.getCpf()));
 		
-		Sinistro contrato_aline = new Sinistro("24/03/2023", "ulaula");
-		System.out.println(contrato_aline.getId());
+		Cliente cliente1 = new Cliente("Aline", "612.977.023-52", "15/02/2003", 19, "Av. Santa Isabel, n. 1125");
+		System.out.println(cliente1.getNome());
+		System.out.println(cliente1.getCpf());
+		System.out.println(cliente1.validarCPF(cliente1.getCpf()));
+		cliente1.setIdade(20);
+		System.out.println(cliente1.getIdade());
+		System.out.print(cliente1.toString());
+		
+		Seguradora seguradora1 = new Seguradora("SafeLife", "+5598984669707", "safelife@life.br", "Av. Alan Turing");
+		System.out.println(seguradora1.getEmail());
+		seguradora1.setEndereco("Av. Ada Lovelace");
+		System.out.println(seguradora1.getEndereco());
+		
+		Sinistro sinistro1 = new Sinistro("27/03/2023", "Rua Aparecida, n. 233");
+		System.out.println(sinistro1.getId());
+		System.out.println(sinistro1.getData());
+		
+		Veiculo veiculo1 = new Veiculo("XP73H", "Hyundai", "Creta 2019");
+		System.out.println(veiculo1.getPlaca());
+		veiculo1.setModelo("flex");
+		System.out.println(veiculo1.getModelo());
 		
 	}
 }
