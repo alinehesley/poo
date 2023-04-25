d775b84665bb60abe4a17452e7afd7581574561d
Aline Hesley <alinehesley281@gmail.com>
2023-04-25 09:30:06 -0300

    funcionando e pronto p criar tag

--- src/Classes/Veiculo.java
+++ src/Classes/Veiculo.java
@@ -26,16 +26,32 @@ public class Veiculo {
 		return placa;
 	}
 
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
 	public String getMarca() {
 		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
 	}
 
 	public String getModelo() {
 		return modelo;
 	}
 
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
 	public int getAnoFabricacao() {
 		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(int anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
 	}
 
 }
